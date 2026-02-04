acca1e4f238393a9cff347c471563caf57c75545
PILIGRIM <dimamishenko2006@gmail.com>
2026-02-05 01:11:09 +0300

    Написал сырые конструкторы для класса vector по правилу 5-ти. Нужжно дописать процедуры swap, copyData, а также подсчёта передаваемых данных

--- vector.hpp
+++ vector.hpp
@@ -13,10 +13,62 @@ size_t capacity_;
 
 public:
 
-/* КОНСТРУКТОРЫ */
+/* конструкторы */
 
 // Конструктор инициализации 
-Vector () : data_(nullptr), size_(0), capacity_(0) {}
+Vector (): data_(nullptr), size_(0), capacity_(0) {}
+
+// Конструктор инициализации с параметрами
+Vector (const  dataType& data): data_(nullptr), size_(0), capacity_(0) {
+    try
+    {
+        if (data){
+          size_ = 1;// функция нахождения длины
+          capacity_ = size_*2 + 1;
+          data_ = new dataType[capacity_];
+          (data_, data) // процедура передачи данных 
+
+        }
+    }
+    catch(const std::bad_alloc& e)
+    {
+        throw std::runtime_error("Memory allocation failed in array (constructor)::reallocate");
+    }
+    
+
+}
+
+// Конструктор копирования
+Vector (const Vector& other) : data_(nullptr), size_(0), capacity_(0){
+    try
+    {
+        if (other.data_){
+            size_ = other.size_;
+            capacity_ = other.capacity_;
+            data_ = new dataType[capacity_];
+            (data_, other.data_) // процкедура передачи данных
+
+
+        }
+    }
+    catch(const std::bad_alloc& e){
+
+        throw std::runtime_error("Memory allocation failed in array (constructor)::reallocate");
+    }
+    
+}
+
+
+// Конструктор перемещения 
+Vector(Vector&& other) noexcept : data_(nullptr), size_(0), capacity_(0){
+    swap(other); // процедура swap
+    
+}
+
+// Десктруктор 
+~Vector() noexcept {
+    delete[] data_;
+}
 
 // геттеры
 
